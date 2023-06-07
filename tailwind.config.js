2257a209c9eadc7e1d4a0044e64f91703105e531
ukps-Heithem Kacem <heithem.kacem@gmail.com>
2023-06-07 20:33:12 +0100

    add : tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#f75200",
+        secondary: "#808080",
+        lightGray: "#6B7280",
+        white: "#FFFFFF",
+        black: "#343434",
+        lightblack: "#36454F",
+        success: "#00b200",
+        fail: "#e50000",
+        avearge: "#FDA50F",
+      },
+    },
+  },
+  plugins: [],
+};

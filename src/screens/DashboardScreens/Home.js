549ba6b9b83191f154e0ac503de04b38b4db6be4
Yassine Zaanouni <yassinezaanouni@gmail.com>
2023-06-08 21:43:10 +0100

    Finished homescreen kinda

--- src/screens/DashboardScreens/Home.js
+++ src/screens/DashboardScreens/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import Layout from "../../components/layout/Layout";
 import DishCategories from "../../components/dishCategories/DishCategories";
 import FilterSelect from "../../components/buttons/FilterSelect";
@@ -32,10 +32,77 @@ const Home = ({ navigation }) => {
         "https://images.unsplash.com/photo-1514326640560-7d063ef2aed5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZGlzaHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
       numberAvailable: 10,
     },
+
     {
-      id: 645645456,
+      id: 645645321456,
       name: "Salad",
       price: 40,
+      image:
+        "https://images.unsplash.com/photo-1514326640560-7d063ef2aed5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZGlzaHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+      numberAvailable: 10,
+    },
+    {
+      id: 456453216456,
+      name: "Burger",
+      price: 10,
+      image:
+        "https://images.unsplash.com/photo-1514326640560-7d063ef2aed5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZGlzaHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+      numberAvailable: 10,
+    },
+    {
+      id: 456442156456,
+      name: "Pizza",
+      price: 20,
+      image:
+        "https://images.unsplash.com/photo-1514326640560-7d063ef2aed5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZGlzaHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+      numberAvailable: 10,
+    },
+
+    {
+      id: 645651245321456,
+      name: "Salad",
+      price: 40,
+      image:
+        "https://images.unsplash.com/photo-1514326640560-7d063ef2aed5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZGlzaHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+      numberAvailable: 10,
+    },
+    {
+      id: 456453212516456,
+      name: "Burger",
+      price: 10,
+      image:
+        "https://images.unsplash.com/photo-1514326640560-7d063ef2aed5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZGlzaHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+      numberAvailable: 10,
+    },
+    {
+      id: 451236442321156456,
+      name: "Pizza",
+      price: 20,
+      image:
+        "https://images.unsplash.com/photo-1514326640560-7d063ef2aed5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZGlzaHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+      numberAvailable: 10,
+    },
+
+    {
+      id: 645645113213214456,
+      name: "Salad",
+      price: 40,
+      image:
+        "https://images.unsplash.com/photo-1514326640560-7d063ef2aed5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZGlzaHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+      numberAvailable: 10,
+    },
+    {
+      id: 45645123332164456,
+      name: "Burger",
+      price: 10,
+      image:
+        "https://images.unsplash.com/photo-1514326640560-7d063ef2aed5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZGlzaHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
+      numberAvailable: 10,
+    },
+    {
+      id: 456442123156456,
+      name: "Pizza",
+      price: 20,
       image:
         "https://images.unsplash.com/photo-1514326640560-7d063ef2aed5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZGlzaHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80",
       numberAvailable: 10,
@@ -75,7 +142,7 @@ const Home = ({ navigation }) => {
             <TouchableOpacity
               activeOpacity={0.9}
               className="bg-primary flex items-center justify-center w-8 h-8 ml-4 rounded-lg"
-              onPress={() => setIsOrdersViewOpen(true)}
+              onPress={() => setIsOrdersViewOpen((prev) => !prev)}
             >
               <MaterialCommunityIcons name="cart" size={20} color="white" />
               <View className="-top-1 -right-1 bg-lightblack absolute items-center justify-center w-4 h-4 rounded-full">
@@ -88,44 +155,48 @@ const Home = ({ navigation }) => {
           <FilterSelect text="Filter" icon="filter" />
         </View>
 
-        <View className="gap-x-8 gap-y-24 flex-row flex-wrap mt-10">
-          {dishes.map((dish, i) => (
-            <View
-              key={i}
-              className="rounded-2xl items-center p-6 bg-lightblack min-w-[250px] pt-[90px]"
-            >
-              <Image
-                source={{ uri: dish.image }}
-                className="w-36 h-36 absolute -translate-y-16 rounded-md"
-              />
-              <Text className="text-lg text-center text-white">
-                {dish.name}
-              </Text>
-              <Text className="mt-2 text-center text-white">${dish.price}</Text>
-              <Text className="text-lightGray mt-1 text-center">
-                {dish.numberAvailable} available
-              </Text>
-              <TouchableOpacity
-                activeOpacity={0.9}
-                style={{
-                  shadowColor: "rgba(192, 132, 252,0.6)",
-                  shadowOffset: {
-                    width: 0,
-                    height: 4,
-                  },
-                  shadowRadius: 4.65,
-                  elevation: 8,
-                }}
-                className="bg-primary w-full p-4 mt-4 rounded-lg"
-                onPress={() => handleOrderNow(dish)}
+        <ScrollView className="">
+          <View className="gap-x-8 gap-y-24 flex-row flex-wrap mt-10">
+            {dishes.map((dish, i) => (
+              <View
+                key={i}
+                className="rounded-2xl items-center p-6 bg-lightblack min-w-[250px] pt-[90px]"
               >
-                <Text className="font-semibold text-center text-white">
-                  Order Now
+                <Image
+                  source={{ uri: dish.image }}
+                  className="w-36 h-36 absolute -translate-y-16 rounded-md"
+                />
+                <Text className="text-lg text-center text-white">
+                  {dish.name}
                 </Text>
-              </TouchableOpacity>
-            </View>
-          ))}
-        </View>
+                <Text className="mt-2 text-center text-white">
+                  ${dish.price}
+                </Text>
+                <Text className="text-lightGray mt-1 text-center">
+                  {dish.numberAvailable} available
+                </Text>
+                <TouchableOpacity
+                  activeOpacity={0.9}
+                  style={{
+                    shadowColor: "rgba(192, 132, 252,0.6)",
+                    shadowOffset: {
+                      width: 0,
+                      height: 4,
+                    },
+                    shadowRadius: 4.65,
+                    elevation: 8,
+                  }}
+                  className="bg-primary w-full p-4 mt-4 rounded-lg"
+                  onPress={() => handleOrderNow(dish)}
+                >
+                  <Text className="font-semibold text-center text-white">
+                    Order Now
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            ))}
+          </View>
+        </ScrollView>
       </Layout>
       {isOrdersViewOpen && (
         <OrdersView

5e8e38884618cb40da32cb736e092acc736d2333
Yassine Zaanouni <yassinezaanouni@gmail.com>
2023-06-10 01:03:01 +0100

    Implemnted add/edit categories

--- src/components/CategoryManagementView/NewCategoryModal.js
+++ src/components/CategoryManagementView/NewCategoryModal.js
@@ -1,15 +1,61 @@
-import { View, Text, TouchableOpacity, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Pressable,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
 import React from "react";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import Animated, { FadeInDown, FadeOutDown } from "react-native-reanimated";
 import { colors } from "../colors";
 import axios from "axios";
 import { API_URL } from "../../util/consts";
+import StyledTextInput from "../inputs/StyledTextInput";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import RegularButton from "../buttons/RegularButton";
+import Toast from "react-native-toast-message";
+
+const newCategorySchema = Yup.object().shape({
+  cat_name: Yup.string()
+    .required("Entrer le nom du catégorie")
+    .min(3, "Le nom du catégorie doit contenir au moins 3 caractères"),
+});
 
 const NewCategoryModal = ({ setIsNewCategoryModalOpen, categoryToUpdate }) => {
+  const createUpdateCategory = async (values, setSubmitting) => {
+    console.log(values);
+    try {
+      const res = await axios.post(
+        `${API_URL}/${
+          categoryToUpdate?._id ? "updateCategory" : `createCategory`
+        }`,
+        {
+          id: categoryToUpdate?._id,
+          ...values,
+        }
+      );
+      Toast.show({
+        type: "success",
+        text1: "Succès",
+        text2: "Nouveau catégorie ajouté avec succès",
+      });
+      setIsNewCategoryModalOpen(false);
+    } catch (error) {
+      Toast.show({
+        type: "error",
+        text1: "Erreur",
+        text2: error.message,
+      });
+    }
+    setSubmitting(false);
+  };
+
   const deleteCategory = async () => {
     try {
-      const res = await axios.post(`${API_URL}/DeleteDish`, {
+      const res = await axios.post(`${API_URL}/deleteCategory`, {
         id: categoryToUpdate._id,
         ...categoryToUpdate,
       });
@@ -18,7 +64,7 @@ const NewCategoryModal = ({ setIsNewCategoryModalOpen, categoryToUpdate }) => {
         text1: "Succès",
         text2: "La catégorie a été supprimée avec succès",
       });
-      setIsNewDishModalOpen(false);
+      setIsNewCategoryModalOpen(false);
     } catch (error) {
       Toast.show({
         type: "error",
@@ -70,6 +116,52 @@ const NewCategoryModal = ({ setIsNewCategoryModalOpen, categoryToUpdate }) => {
           >
             <MaterialCommunityIcons name="close" size={24} color="white" />
           </TouchableOpacity>
+        </View>
+
+        <View className="flex-1 mt-10">
+          <Formik
+            initialValues={{ cat_name: categoryToUpdate?.cat_name || "" }}
+            validationSchema={newCategorySchema}
+            onSubmit={(values, { setSubmitting }) => {
+              console.log("values");
+              createUpdateCategory(values, setSubmitting);
+            }}
+          >
+            {({
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              values,
+              isSubmitting,
+              errors,
+              touched,
+            }) => (
+              <>
+                <ScrollView className="my-8">
+                  <View className="">
+                    <StyledTextInput
+                      icon="food-variant"
+                      label={"Nom du catégorie"}
+                      placeholder={"Entrer le nom du catégorie"}
+                      onChangeText={handleChange("cat_name")}
+                      onBlur={handleBlur("cat_name")}
+                      style={{ marginBottom: 25 }}
+                      value={values.cat_name}
+                      errors={touched.cat_name && errors.cat_name}
+                    />
+                  </View>
+                </ScrollView>
+                {!isSubmitting && (
+                  <RegularButton onPress={handleSubmit}>Valider</RegularButton>
+                )}
+                {isSubmitting && (
+                  <RegularButton disabled={true}>
+                    <ActivityIndicator size="small" color={colors.white} />
+                  </RegularButton>
+                )}
+              </>
+            )}
+          </Formik>
         </View>
       </Animated.View>
     </>

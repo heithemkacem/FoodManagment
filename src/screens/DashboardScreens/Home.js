565d69c4c01064df7c94dd0df7e2dba0f50a1808
ukps-Heithem Kacem <heithem.kacem@gmail.com>
2023-06-12 16:56:27 +0100

    add : printer

--- src/screens/DashboardScreens/Home.js
+++ src/screens/DashboardScreens/Home.js
@@ -37,7 +37,29 @@ const Home = ({ navigation }) => {
     setOrders(newOrders);
   };
   const handlePrintTicket = async () => {
-    const ticketHTML = `<html><body><h1>Restaurant Order Ticket</h1><p>${orders}</p></body></html>`;
+    //ticket html template contain the order details and the total price of the order
+    const ticketHTML = `
+    <div style="width: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;">
+      <h1 style="font-size: 1.5rem; font-weight: bold; margin-bottom: 1rem;">Ticket</h1>
+      <div style="width: 100%; display: flex; flex-direction: column; align-items: center; justify-content: center;">
+        ${orders.map(
+          (order) => `
+          <div style="width: 100%; display: flex; flex-direction: row; align-items: center; justify-content: space-between; margin-bottom: 1rem;">
+            <p style="font-size: 1rem; font-weight: bold;">${order.name}</p>
+            <p style="font-size: 1rem; font-weight: bold;">${order.quantity}</p>
+          </div>
+        `
+        )}
+      </div>
+      <div style="width: 100%; display: flex; flex-direction: row; align-items: center; justify-content: space-between; margin-bottom: 1rem;">
+        <p style="font-size: 1rem; font-weight: bold;">Total</p>
+        <p style="font-size: 1rem; font-weight: bold;">${orders.reduce(
+          (acc, order) => acc + order.price * order.quantity,
+          0
+        )} DT</p>
+      </div>
+    </div>
+  `;
 
     try {
       const { uri } = await Print.printToFileAsync({ html: ticketHTML });

08bde8c05ef3272f1c0d1319aba2f67a9bdc6bed
ukps-Heithem Kacem <heithem.kacem@gmail.com>
2023-06-12 14:28:40 +0100

    fix : categories dish style

--- src/components/dishCategories/DishCategories.js
+++ src/components/dishCategories/DishCategories.js
@@ -17,20 +17,20 @@ const DishCategories = ({
   }, []);
 
   return (
-    <View className="border-gray-100/10 flex-row flex-wrap w-full pb-3 space-x-6 border-b">
+    <View className="border-gray-100/10 flex-row flex-wrap w-full  border-b h-fit justify-evenly  ">
       <TouchableOpacity
         activeOpacity={0.8}
         onPress={() => setSelectedCategoryId(null)}
       >
         <Text
-          className={`${smallText ? "text-md" : "text-lg"} ${
+          className={`${smallText ? "text-md" : "text-lg"} p-1 ${
             selectedCategoryId == null ? "text-primary" : "text-white"
           }`}
         >
           Tous
         </Text>
         <View
-          className={`w-full h-1 absolute -bottom-[14px] ${
+          className={`w-full${
             selectedCategoryId == null ? "bg-primary" : "bg-transparent"
           }`}
         />
@@ -42,14 +42,14 @@ const DishCategories = ({
           onPress={() => setSelectedCategoryId(category._id)}
         >
           <Text
-            className={`${smallText ? "text-md" : "text-lg"} ${
+            className={`${smallText ? "text-md" : "text-lg"} p-1   ${
               selectedCategoryId == category._id ? "text-primary" : "text-white"
             }`}
           >
             {category.cat_name}
           </Text>
           <View
-            className={`w-full h-1 absolute -bottom-[14px] ${
+            className={`w-full  ${
               category._id == selectedCategoryId
                 ? "bg-primary"
                 : "bg-transparent"
